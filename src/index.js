0b32c01503935bc7f78c02ce6cbd5d1d2d7ef83a
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-09-06 01:10:51 +0300

    conditioning for creating new element

--- src/index.js
+++ src/index.js
@@ -16,18 +16,18 @@ document.addEventListener("DOMContentLoaded", () => {
   // Add task
   function addTask(e) {
     e.preventDefault();
-/*
-  // get input value
-  const newTaskDescription = document.getElementById('new-task-description').value;
-  console.log(newTaskDescription);
 
-  // Create new li element
-  const li = document.createElement('li');
+    // get input value
+    const newTaskDescription = document.getElementById('new-task-description').value;
+    console.log(newTaskDescription);
 
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newTaskDescription));
+    // Create new li element
+    const li = document.createElement('li');
 
-  // create delete button element
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newTaskDescription));
+
+/*  // create delete button element
   const deleteBtn = document.createElement('button');
 
   // append text node to delete button
